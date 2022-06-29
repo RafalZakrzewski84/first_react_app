b60f4fdd048c2805f5133c812fd17e2b3e8f1505
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-29 20:47:17 +0200

    adding attributes for anchor tag

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,16 +1,21 @@
+/** @format */
+
 // 1. Article.js
 // Element li
 // w elemencie li:
 // a w którym zawierać ma się tag img, span z przykładowym tekstem
 
-import React from "react";
+import React from 'react';
 
-function Article () {
-    return (
-        <li>
-            <a><img /><span>Link with img and description</span></a>
-        </li>
-    )
+function Article() {
+	return (
+		<li>
+			<a href='#'>
+				<img src='/' alt='alt' />
+				<span>Link with img and description</span>
+			</a>
+		</li>
+	);
 }
 
 export default Article;
