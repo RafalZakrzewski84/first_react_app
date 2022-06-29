151bdd81d0d7d4dd84c4c246aa2f35dbd037f7ad
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-29 19:46:46 +0200

    cleaning app for further  development

--- src/App.js
+++ src/App.js
@@ -2,27 +2,13 @@
 
 import React, { Component, useState } from 'react';
 import Navbar from './components/navbar/Navbar';
-import Counter from './components/counter/Counter';
-
-// class App extends Component {
-// 	render() {
-// 		return (
-// 			<div className='App'>
-// 				<h1>Hello World</h1>
-// 			</div>
-// 		);
-// 	}
-// }
 
 function App() {
 	const [counter, setCounter] = useState(0);
 
 	return (
 		<div className='App'>
-			<span>Counter in app.js: {counter}</span>
-			{/* passing counter and setCounter to lower element navbar */}
-			<Navbar counter={counter} setCounter={setCounter} />
-			<Counter counter={counter}/>
+			<Navbar />
 		</div>
 	);
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,14 @@
 /** @format */
 
 import React from 'react';
-//zadanie
-// W Navbar.js dodaj Button który będzie aktualizował stan counter w App.js i wyświetlaj ten counter w Navbar.js w spanie
-// Przekaż stan counter z App.js do Counter.js i tam go wyświetl
 
-const Navbar = (props) => {
-	console.log(props);
-
-	//setting counter variable from app.js using its setting function from passed props
-	const counterAppAdding = () => {
-		props.setCounter(props.counter + 1);
-	};
+const Navbar = () => {
 	return (
 		<nav>
 			<h1>SDA News</h1>
 			<button>Home</button>
 			<button>Search</button>
 			<button>Login</button>
-			<button onClick={counterAppAdding}>Add 1 to app counter</button>
-			<div>
-				{/* printing value of counter from app.ja */}
-				<p>Counter from app.js: {props.counter}</p>
-				
-			</div>
 		</nav>
 	);
 };
