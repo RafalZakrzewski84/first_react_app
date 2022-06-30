2d94df8005f260257929cb6cb1734ee7d3eba86c
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-30 20:22:25 +0200

    adding axios request to api, api key is in helpers.js

---
+++ src/components/helpers/helpers.js
@@ -0,0 +1,3 @@
+const API_KEY = '692cb1036c634408b47f76340e545e10'
+
+export default API_KEY;
--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -4,9 +4,34 @@
 // h2 Today's hottest news:
 // pusty ul
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import API_KEY from '../helpers/helpers';
 
 function HomePage() {
+	//hook use state
+	const [todaysArticles, setTodaysArticles] = useState([]);
+	//hook use effect
+	useEffect(() => {
+		const today = new Date();
+		const day = today.getDate();
+		const month = today.getMonth();
+		const year = today.getFullYear();
+
+		//feaching data from news API using axios
+		axios
+			.get(
+				`https://newsapi.org/v2/everything?q=world&from=${year}-${
+					month < 10 ? `0${month + 1}` : month + 1
+				}-${day - 1}&language=en&sortBy=popularity&apiKey=${API_KEY}`
+			)
+			.then((response) => {
+				console.log(response);
+				setTodaysArticles(response.data.articles);
+			})
+			.catch((e)=>{console.log(e)});
+	}, []);
+
 	return (
 		<>
 			<h2>Today's hottest news:</h2>
