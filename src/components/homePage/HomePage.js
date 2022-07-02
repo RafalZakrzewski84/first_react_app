fbb0833765250f47c52cd7fa79a1325e520a5c46
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-02 14:27:05 +0200

    styling home page

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -5,8 +5,13 @@
 // pusty ul
 
 import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 import axios from 'axios';
 import API_KEY from '../helpers/helpers';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
 import Article from '../article/Article';
 
 function HomePage() {
@@ -36,16 +41,35 @@ function HomePage() {
 			});
 	}, []);
 
+	// 1. h2 zamień na Typography (sprawdź jak wyświetlić Tg jako h2), text: Today's hottest news:
+	//2. ul => List
 	return (
-		<>
-			<h2>Today's hottest news:</h2>
-			{/* rendering articles on page, we are passing data to article by prop art */}
-			<ul>{todaysArticles.map((art, idx) => {
-				return <Article key={idx} art={art}/>
-			})}
-			</ul>
-		</>
+		<Container maxWidth="sm" sx={{}}>
+			<Typography
+				variant="h2"
+				component="h2"
+				align="center"
+				sx={{ fontSize: '2rem', my: 2 }}>
+				Today's hottest news
+			</Typography>
+			<List>
+				{todaysArticles.map((art, idx) => {
+					return <Article key={idx} art={art} />;
+				})}
+			</List>
+		</Container>
 	);
 }
 
 export default HomePage;
+
+//original code before styling
+// <>
+// 	<h2>Today's hottest news:</h2>
+// 	{/* rendering articles on page, we are passing data to article by prop art */}
+// 	<ul>
+// 		{todaysArticles.map((art, idx) => {
+// 			return <Article key={idx} art={art} />;
+// 		})}
+// 	</ul>
+// </>
