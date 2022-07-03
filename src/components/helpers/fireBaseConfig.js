d3fd9105b432b3816dd4432bd290341206c3a1de
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-03 10:37:52 +0200

    adding firebase config file

---
+++ src/components/helpers/fireBaseConfig.js
@@ -0,0 +1,43 @@
+/** @format */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js';
+import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-analytics.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-storage.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.8.2/firebase-firestore.js';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDEAy30tUasXEuP2uLajilyR8KNaeVAn7U',
+	authDomain: 'sdanews-acd6d.firebaseapp.com',
+	projectId: 'sdanews-acd6d',
+	storageBucket: 'sdanews-acd6d.appspot.com',
+	messagingSenderId: '681013709198',
+	appId: '1:681013709198:web:26efb2524a2123c1917cdf',
+	measurementId: 'G-23S3FLXQ2V',
+	databaseURL:
+		'https://sdanews-acd6d-default-rtdb.europe-west1.firebasedatabase.app',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const database = getDatabase();
+const storage = getStorage(app);
+const fireStore = getFirestore(app);
+
+export default {
+	app,
+	analytics,
+	auth,
+	database,
+	storage,
+	fireStore,
+};
