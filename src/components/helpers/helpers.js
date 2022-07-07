575ce484a694120d450c67c38c027119ca031514
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-07 10:58:32 +0200

    adding new key to new API

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,3 +1,9 @@
-const API_KEY = '692cb1036c634408b47f76340e545e10'
+/** @format */
+
+//old news api
+// const API_KEY = '692cb1036c634408b47f76340e545e10'
+
+//new https://mediastack.com/ api
+const API_KEY = '059baa7fbfd79ba5c670e5ebc3ff3205';
 
 export default API_KEY;
